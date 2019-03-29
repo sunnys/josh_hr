1eaa03f7067b173a98958a4c9ed1a0fef1c9c344
sunny <sunny@quodeck.com>
2019-03-30 00:02:36 +0530

    added schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_151657) do
+ActiveRecord::Schema.define(version: 2019_03_29_141750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -225,6 +225,13 @@ ActiveRecord::Schema.define(version: 2019_03_28_151657) do
     t.string "title"
     t.string "caption"
     t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "main_page_images", force: :cascade do |t|
+    t.string "pic1"
+    t.string "pic2"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
